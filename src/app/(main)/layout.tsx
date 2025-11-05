a70d7e8f18db47c17114dea525cce7d61f1e690a
Tharun kumar <tharunriot@gmail.com>
2025-11-05 16:44:11 +0000

    buddy were is the menu bar ?

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,7 +1,7 @@
 import { Footer } from "@/components/layout/Footer";
 import { Header } from "@/components/layout/Header";
 
-export default function SiteLayout({
+export default function MainLayout({
   children,
 }: {
   children: React.ReactNode;
